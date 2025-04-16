5903be1a3f5a0b93e769543d2690c4a4c60c6014
Stefan Panaite <stefanpanaite@mac.home>
2025-04-16 12:04:03 +0300

    corectie app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import MetaBalls from './MetaBalls';
 
-<MetaBalls
-  color="#ffffff"
-  cursorBallColor="#ffffff"
-  cursorBallSize={2}
-  ballCount={15}
-  animationSize={30}
-  enableMouseInteraction={true}
-  enableTransparency={true}
-  hoverSmoothness={0.05}
-  clumpFactor={1}
-  speed={0.3}
-/>
+function App() {
+  return (
+    <div className="App">
+      <MetaBalls
+        color="#ffffff"
+        cursorBallColor="#ffffff"
+        cursorBallSize={2}
+        ballCount={15}
+        animationSize={30}
+        enableMouseInteraction={true}
+        enableTransparency={true}
+        hoverSmoothness={0.05}
+        clumpFactor={1}
+        speed={0.3}
+      />
+    </div>
+  );
+}
+
+export default App;
